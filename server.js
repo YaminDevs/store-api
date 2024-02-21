d7856f47277403e8b5182e76c96c5c9bde24051e
EshuDevs <yamindevs@gmail.com>
2024-02-21 14:14:47 +0100

    build the /login and /profile/:id endpoints

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
 const knex = require('knex');
-const pg = knex({
+const postgres = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
@@ -18,6 +18,19 @@ const pg = knex({
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/uploads", express.static("./uploads"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads'); 
+  },
+  filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
 
 const PORT = process.env.PORT || 3001; 
 
@@ -48,7 +61,7 @@ app.post('/register', async (req, res) => {
             email: email,
             name: name,
             password: hash,
-            created: new Date()
+            joined: new Date()
           })
           .into('users')
           .returning('*');
@@ -61,12 +74,54 @@ app.post('/register', async (req, res) => {
     }
   });
 
-app.post('/login', (req, res) => {
-    res.send('Hello, world!'); 
-});
+  app.post('/login', async (req, res) => {
+    try {
+      const loginData = await postgres
+        .select('email', 'hash')
+        .from('login')
+        .where('email', '=', req.body.email);
+  
+      if (loginData.length === 0) {
+        return res.status(400).json('wrong credentials');
+      }
+  
+      const { email, hash } = loginData[0];
+      const isValid = await bcrypt.compare(req.body.password, hash);
+  
+      if (isValid) {
+        const userData = await postgres
+          .select('*')
+          .from('users')
+          .where('email', '=', email);
+  
+        res.json(userData[0]);
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(400).json('unable to login');
+    }
+  });
 
-app.get('/profile:id', (req, res) => {
-    res.send('Hello, world!'); 
+  app.get('/profile/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await postgres
+            .select('*')
+            .from('users')
+            .where('user_id', id)
+            .first();
+
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        console.error('Error retrieving user profile:', error);
+        res.status(500).json({ error: 'Error retrieving user profile' });
+    }
 });
 
 app.get('/cart', (req, res) => {
