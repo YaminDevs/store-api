88288ade5ca2fbc534bd116ae24cb51d6b793d3b
EshuDevs <yamindevs@gmail.com>
2024-02-22 10:57:30 +0100

    updated the /items endpoint so it can filter for specifc categories

--- server.js
+++ server.js
@@ -214,14 +214,29 @@ app.post('/orders', async (req, res) => {
     res.json(orders[0]);
 });
 
-app.get('/items', async  (req, res) => {
-    try{
-        const items = await postgres
-        .select('*')
-        .from('items');
+app.get('/items', async (req, res) => {
+    try {
+        let items;
+
+        // Check if a category filter is specified in the query parameters
+        const category = req.query.category;
+        if (category) {
+            // If a category is specified, filter items based on the category
+            items = await postgres
+                .select('items.*')
+                .from('items')
+                .innerJoin('item_categories', 'items.item_id', 'item_categories.item_id')
+                .innerJoin('categories', 'item_categories.category_id', 'categories.category_id')
+                .where('categories.category_name', category);
+        } else {
+            // If no category is specified, retrieve all items
+            items = await postgres
+                .select('*')
+                .from('items');
+        }
+
         res.json(items);
-    }
-    catch (error) {
+    } catch (error) {
         console.error('Error retrieving items:', error);
         res.status(500).json({ error: 'Error retrieving items' });
     }
